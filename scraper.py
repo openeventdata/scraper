5d87650709bea836558f778124a7a1ab5cfa8e1f
John Beieler <john.b30@gmail.com>
2014-02-27 01:18:31 -0500

    Refactoring and bug hunting

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 import os
 import re
 import glob
-import time
 import logging
 import pattern.web
 import pages_scrape
@@ -9,23 +8,21 @@ import mongo_connection
 from goose import Goose
 from pymongo import MongoClient
 from ConfigParser import ConfigParser
-from apscheduler.scheduler import Scheduler
 
 
 def scrape_func(address, website, COLL):
     """
-    Function to scrape various RSS feeds. Uses the 'keep' and 'ignore'
-    iterables to define which words should be used in the text search.
+    Function to scrape various RSS feeds.
 
     Parameters
     ------
-    address : String
+    address: String.
                 Address for the RSS feed to scrape.
 
-    name : String
-            Nickname for the RSS feed being scraped.
+    website: String.
+                Nickname for the RSS feed being scraped.
 
-    COLL : String
+    COLL: String.
             Collection within MongoDB that holds the scraped data.
     """
     #Setup the database
@@ -34,6 +31,36 @@ def scrape_func(address, website, COLL):
     collection = db[COLL]
 
     #Scrape the RSS feed
+    results = _get_rss(address, website)
+
+    #Pursue each link in the feed
+    if results:
+        _parse_results(results, website, collection)
+
+    logger.info('Scrape of {} finished'.format(website))
+
+
+def _get_rss(address, website):
+    """
+    Private function to parse an RSS feed and extract the relevant links.
+
+    Parameters
+    ------
+    address: String.
+                Address for the RSS feed to scrape.
+
+    website: String.
+                Nickname for the RSS feed being scraped.
+
+    Returns
+    -------
+
+    results : pattern.web.Results.
+                Object containing data on the parsed RSS feed. Each item
+                represents a unique entry in the RSS feed and contains relevant
+                information such as the URL and title of the story.
+
+    """
     try:
         results = pattern.web.Newsfeed().search(address, count=100,
                                                 cached=False)
@@ -44,46 +71,101 @@ def scrape_func(address, website, COLL):
         logger.warning('Problem fetching RSS feed for {}. {}'.format(address,
                                                                      e))
         results = None
-    #Pursue each link in the feed
-    if results:
-        goose_extractor = Goose({'use_meta_language': False,
-                                 'target_language': 'en'})
-        for result in results:
-            if website == 'xinhua':
-                page_url = result.url.replace('"', '')
-                page_url = page_url.encode('ascii')
-            elif website == 'upi':
-                page_url = result.url.encode('ascii')
-            else:
-                page_url = result.url
 
-            try:
-                text, meta = pages_scrape.scrape(page_url, goose_extractor)
-                text = text.encode('utf-8')
-            except TypeError:
-                logger.warning('Problem obtaining text from URL: {}'.format(page_url))
-                text = ''
+    return results
 
-            if text:
-                if website == 'bbc':
-                    text = text.replace("This page is best viewed in an up-to-date web browser with style sheets (CSS) enabled. While you will be able to view the content of this page in your current browser, you will not be able to get the full visual experience. Please consider upgrading your browser software or enabling style sheets (CSS) if you are able to do so.", '')
-                if website == 'almonitor':
-                    text = re.sub("^.*?\(photo by REUTERS.*?\)", "", text)
-                if website == 'menafn_algeria' or website == 'menafn_bahrain' or website == 'menafn_egypt' or website == 'menafn_iraq' or website == 'menafn_jordan' or website == 'menafn_kuwait' or website == 'menafn_lebanon' or website == 'menafn_morocco' or website == 'menafn_oman' or website == 'menafn_palestine' or website == 'menafn_qatar' or website == 'menafn_saudi' or website == 'menafn_syria' or website == 'menafn_tunisia' or website == 'menafn_turkey' or website == 'menafn_uae' or website == 'menafn_yemen':
-                    text = re.sub("^\(.*?MENAFN.*?\)", "", text)
-                elif website == 'upi':
-                    text = text.replace("Since 1907, United Press International (UPI) has been a leading provider of critical information to media outlets, businesses, governments and researchers worldwide. UPI is a global operation with offices in Beirut, Hong Kong, London, Santiago, Seoul and Tokyo. Our headquarters is located in downtown Washington, DC, surrounded by major international policy-making governmental and non-governmental organizations. UPI licenses content directly to print outlets, online media and institutions of all types. In addition, UPI's distribution partners provide our content to thousands of businesses, policy groups and academic institutions worldwide. Our audience consists of millions of decision-makers who depend on UPI's insightful and analytical stories to make better business or policy decisions. In the year of our 107th anniversary, our company strives to continue being a leading and trusted source for news, analysis and insight for readers around the world.", '')
 
-                entry_id = mongo_connection.add_entry(collection, text,
-                                                      result.title, result.url,
-                                                      result.date, website)
-                if entry_id:
-                    logger.info('Added entry from {} with id {}'.format(result.url,
+def _parse_results(rss_results, website, db_collection):
+    """
+    Private function to parse the links drawn from an RSS feed.
+
+    Parameters
+    ------
+    rss_results: pattern.web.Results.
+                    Object containing data on the parsed RSS feed. Each item
+                    represents a unique entry in the RSS feed and contains
+                    relevant information such as the URL and title of the
+                    story.
+
+    website: String.
+                Nickname for the RSS feed being scraped.
+
+    db_collection: pymongo Collection.
+                        Collection within MongoDB that in which results are
+                        stored.
+    """
+    goose_extractor = Goose({'use_meta_language': False,
+                             'target_language': 'en'})
+
+    for result in rss_results:
+        if website == 'xinhua':
+            page_url = result.url.replace('"', '')
+            page_url = page_url.encode('ascii')
+        elif website == 'upi':
+            page_url = result.url.encode('ascii')
+        else:
+            page_url = result.url.encode('utf-8')
+
+        try:
+            text, meta = pages_scrape.scrape(page_url, goose_extractor)
+            text = text.encode('utf-8')
+        except TypeError:
+            logger.warning('Problem obtaining text from URL: {}'.format(page_url))
+            text = ''
+
+        if text:
+            cleaned_text = _clean_text(text, website)
+
+            entry_id = mongo_connection.add_entry(db_collection, cleaned_text,
+                                                  result.title, result.url,
+                                                  result.date, website)
+            if entry_id:
+                try:
+                    logger.info('Added entry from {} with id {}'.format(page_url,
                                                                         entry_id))
-                else:
-                    logger.info('Result from {} already in database'.format(result.url,
-                                                                            entry_id))
-    logger.info('Scrape of {} finished'.format(website))
+                except UnicodeDecodeError:
+                    logger.info('Added entry from {}. Unicode error for id'.format(result.url))
+            else:
+                logger.info('Result from {} already in database'.format(page_url))
+
+
+def _clean_text(text, website):
+    """
+    Private function to clean some of the cruft from the content pulled from
+    various sources.
+
+    Parameters
+    --------
+
+    text: String.
+            Dirty text.
+
+    website: String.
+                Nickname for the RSS feed being scraped.
+
+    Returns
+    ------
+
+    text: String.
+            Less dirty text.
+    """
+    site_list = ['menafn_algeria', 'menafn_bahrain', 'menafn_egypt',
+                 'menafn_iraq', 'menafn_jordan', 'menafn_kuwait',
+                 'menafn_lebanon', 'menafn_morocco', 'menafn_oman',
+                 'menafn_palestine', 'menafn_qatar', 'menafn_saudi',
+                 'menafn_syria', 'menafn_tunisia', 'menafn_turkey',
+                 'menafn_uae', 'menafn_yemen']
+
+    if website == 'bbc':
+        text = text.replace("This page is best viewed in an up-to-date web browser with style sheets (CSS) enabled. While you will be able to view the content of this page in your current browser, you will not be able to get the full visual experience. Please consider upgrading your browser software or enabling style sheets (CSS) if you are able to do so.", '')
+    if website == 'almonitor':
+        text = re.sub("^.*?\(photo by REUTERS.*?\)", "", text)
+    if website in site_list:
+        text = re.sub("^\(.*?MENAFN.*?\)", "", text)
+    elif website == 'upi':
+        text = text.replace("Since 1907, United Press International (UPI) has been a leading provider of critical information to media outlets, businesses, governments and researchers worldwide. UPI is a global operation with offices in Beirut, Hong Kong, London, Santiago, Seoul and Tokyo. Our headquarters is located in downtown Washington, DC, surrounded by major international policy-making governmental and non-governmental organizations. UPI licenses content directly to print outlets, online media and institutions of all types. In addition, UPI's distribution partners provide our content to thousands of businesses, policy groups and academic institutions worldwide. Our audience consists of millions of decision-makers who depend on UPI's insightful and analytical stories to make better business or policy decisions. In the year of our 107th anniversary, our company strives to continue being a leading and trusted source for news, analysis and insight for readers around the world.", '')
+
+    return text
 
 
 def call_scrape_func(siteList, db_collection):
@@ -93,7 +175,7 @@ def call_scrape_func(siteList, db_collection):
     Parameters
     ----------
 
-    siteList : dictionary
+    siteList: dictionary
                 Dictionary of sites, with a nickname as the key and RSS URL
                 as the value.
     """
@@ -135,7 +217,7 @@ if __name__ == '__main__':
     logger = logging.getLogger('scraper_log')
     logger.setLevel(logging.INFO)
 
-    fh = logging.FileHandler('scraping_log.log', 'w')
+    fh = logging.FileHandler('scraping_log.log', 'a')
     formatter = logging.Formatter('%(levelname)s %(asctime)s: %(message)s')
     fh.setFormatter(formatter)
 
@@ -155,14 +237,4 @@ if __name__ == '__main__':
         print 'There was an error. Check the log file for more information.'
         logger.warning('Could not open URL whitelist file.')
 
-    #Line to aid in debugging
-    #call_scrape_func(to_scrape, db_collection)
-
-    #Run the `scrape_func` once each hour
-    sched = Scheduler()
-    sched.add_interval_job(call_scrape_func, args=[to_scrape, db_collection],
-                           hours=1)
-    sched.start()
-    while True:
-        time.sleep(10)
-    sched.shutdown()
+    call_scrape_func(to_scrape, db_collection)
